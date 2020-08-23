550da836ddeb5b660445d1a97da0a68e6b5abf2b
Antigona <antigona.sherifi@hotmail.com>
2020-08-23 12:36:19 +0200

    First commit

---
+++ try.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+5.times do
+    print "Hello! This is an example \n"
+end
+
+# Execute this code:
+
+# Make sure you are in the right directory: 
+# cd /Users/anti/code/start-ruby
+
+# Execute Code:
+# ruby try.rb
+
+# Execute Code from another location:
+# ruby /Users/anti/code/start-ruby/try.rb
---
+++ try_numbers.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+cool_number = 34 * 6
+
+print cool_number
+
